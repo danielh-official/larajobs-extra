234eca7ccb8f0c7679e7dfc6e66a96de565a3cee
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-03 23:34:12 -0500

    Refactor loading state management and enhance filter functionality in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,17 @@
 	import { liveQuery } from 'dexie';
 	import { onMount } from 'svelte';
 	import convert from 'xml-js';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
 
 	let content = $state('');
 	let jsonContent: LaraJobsFeedXml | null = $state(null);
 	let parseError: string | null = $state(null);
 
-	let showParseRssContent = $state(true);
+	let loading = $state(false);
+
+	let showParseRssContent = $state(false);
 
 	function setShowParseRssContent(value: boolean) {
 		showParseRssContent = value;
@@ -22,10 +27,21 @@
 		localStorage.setItem('showFilters', value.toString());
 	}
 
-	onMount(() => {
-		showParseRssContent = localStorage.getItem('showParseRssContent') === 'true' ? true : false;
-		showFilters = localStorage.getItem('showFilters') === 'true' ? true : false;
+	// MARK: - onMount
+
+	onMount(async () => {
+		loading = true;
+
+		if (browser) {
+			await new Promise((resolve) => setTimeout(resolve, 500));
+			showParseRssContent = localStorage.getItem('showParseRssContent') === 'true' ? true : false;
+			showFilters = localStorage.getItem('showFilters') === 'true' ? true : false;
+		}
+
+		loading = false;
 	});
+
+	// MARK: - RSS Parsing and Database Writing
 
 	interface LaraJobsFeedXml {
 		rss: {
@@ -184,6 +200,8 @@
 		});
 	}
 
+	// MARK: - Utility Functions
+
 	function getDateInRelativeFormat(date: Date | null | undefined): string {
 		if (!date) return 'Unknown date';
 
@@ -212,231 +230,300 @@
 		return 'Just now';
 	}
 
-	let company = $state<string>('');
+	// MARK: - Filters
 
-	let tags = $state<string>('');
+	let company = $state<string>(page.url.searchParams.get('company') || '');
 
-	let parsedTags = $derived.by(() => {
-		return tags
-			.split(',')
-			.map((tag) => tag.trim().toLowerCase())
-			.filter((tag) => tag.length > 0);
+	let queryParamCompany = $derived.by(() => {
+		return page.url.searchParams.get('company') || '';
 	});
 
+	let tags = $state<string>(page.url.searchParams.get('tags') || '');
+
+	let queryParamTags = $derived.by(() => {
+		return page.url.searchParams.get('tags') || '';
+	});
+
+	let parsedTags = $derived.by(() => {
+		const tagsParam = page.url.searchParams.get('tags') || '';
+		return tagsParam
+			.split(',')
+			.map((tag) => tag.trim().toLowerCase())
+			.filter((tag) => tag !== '');
+	});
+
+	let publishedDate = $state<string>(page.url.searchParams.get('published_date') || '');
+
+	let queryParamPublishedDate = $derived.by(() => {
+		return page.url.searchParams.get('published_date') || '';
+	});
+
+	function applyFilters() {
+		const url = new URL(page.url);
+		company && url.searchParams.set('company', company);
+		tags && url.searchParams.set('tags', tags);
+		publishedDate && url.searchParams.set('published_date', publishedDate);
+
+		if (!company) {
+			url.searchParams.delete('company');
+		}
+
+		if (!tags) {
+			url.searchParams.delete('tags');
+		}
+
+		if (!publishedDate) {
+			url.searchParams.delete('published_date');
+		}
+
+		goto(url.toString(), { keepFocus: true });
+	}
+
+	// MARK: - Postings List
+
 	const postings = $derived.by(() => {
-		company;
-		parsedTags;
+		queryParamCompany;
+		queryParamTags;
+    queryParamPublishedDate;
 
 		return liveQuery(async () => {
 			let collection = db.postings.toCollection();
 
-			if (company.trim() !== '') {
+			if (queryParamCompany.trim() !== '') {
 				collection = collection.filter((posting) =>
 					posting.company.toLowerCase().includes(company.trim().toLowerCase())
 				);
 			}
 
 			if (parsedTags.length > 0) {
-				collection = collection.filter((posting) => {
-					const postingTags = posting.tags.map((tag) => tag.toLowerCase());
-					return parsedTags.every((tag) => postingTags.includes(tag));
-				});
+				if (parsedTags.length === 1 && parsedTags[0] === 'none') {
+					collection = collection.filter((posting) => {
+						return (
+							posting.tags.length === 0 || (posting.tags.length === 1 && posting.tags[0] === '')
+						);
+					});
+				} else {
+					collection = collection.filter((posting) => {
+						const postingTags = posting.tags.map((tag) => tag.toLowerCase());
+						return parsedTags.every((tag) => postingTags.includes(tag));
+					});
+				}
 			}
+
+      if (queryParamPublishedDate.trim() !== '') {
+        // Currently, we are only sorting by published date in descending order.
+        // Additional filtering logic can be added here if needed in the future.
+      }
 
 			return await collection.sortBy('parsed_published_date').then((results) => results.reverse());
 		});
 	});
-
-	$inspect(postings);
-
-	function deletePosting(id: number) {
-		if (!confirm('Are you sure you want to delete this posting?')) {
-			return;
-		}
-
-		db.postings.delete(id).catch((error) => {
-			console.error('Error deleting posting:', error);
-		});
-	}
 </script>
 
-<!-- Top Items -->
-<div class="flex items-center mt-6 gap-x-4 justify-between max-w-4xl mx-auto">
-	<div class="flex gap-x-4">
-		<button
-			class="px-4 py-2 rounded-lg border-2 text-white hover:bg-white cursor-pointer bg-red-500 border-red-500 hover:text-red-500"
-			onclick={() => setShowParseRssContent(!showParseRssContent)}
-		>
-			{showParseRssContent ? 'Hide RSS Parser' : 'Show RSS Parser'}
-		</button>
-		<button
-			class="px-4 py-2 rounded-lg border-2 text-white hover:bg-white cursor-pointer bg-red-500 border-red-500 hover:text-red-500"
-			onclick={() => setShowFilters(!showFilters)}
-		>
-			{showFilters ? 'Hide Filters' : 'Show Filters'}
-		</button>
+{#if loading}
+	<div class="flex justify-center items-center h-screen">
+		<div
+			class="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"
+		></div>
 	</div>
-	<div class="flex gap-x-4">
-		<a
-			href="https://larajobs.com"
-			class="hover:underline cursor-pointer"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			Visit LaraJobs
-		</a>
-	</div>
-</div>
-
-{#if showParseRssContent}
-	<div class="max-w-4xl mx-auto my-6 p-4 border rounded shadow">
-		<h2 class="text-xl font-bold mb-4">Instructions to Parse RSS Feed</h2>
-		<ol class="list-decimal list-inside space-y-2">
-			<li>
-				Go to <a
-					href="https://larajobs.com/feed"
-					class="text-blue-500 hover:text-blue-700"
-					target="_blank">https://larajobs.com/feed</a
-				>
-			</li>
-			<li>
-				Copy all the contents under "This XML file does not appear to have any style information
-				associated with it. The document tree is shown below."
-			</li>
-			<li>Paste the content into the text box below</li>
-			<li>Click "Parse RSS Content" to add the job postings</li>
-		</ol>
-	</div>
-	<div class="my-6 text-center flex flex-col gap-y-2">
-		<div>
-			<textarea bind:value={content} rows="4" cols="80" placeholder="Paste RSS feed content here"
-			></textarea>
-		</div>
-		<div>
+{:else}
+	<!-- MARK: - Top Items -->
+	<div class="flex items-center mt-6 gap-x-4 justify-between max-w-4xl mx-auto">
+		<div class="flex gap-x-4">
 			<button
-				class="px-6 py-4 rounded-lg border-2 text-white hover:bg-white bg-red-500 cursor-pointer border-red-500 hover:text-red-500"
-				onclick={parseRssContent}
+				class="px-4 py-2 rounded-lg border-2 text-white hover:bg-white cursor-pointer bg-red-500 border-red-500 hover:text-red-500"
+				onclick={() => setShowParseRssContent(!showParseRssContent)}
 			>
-				Parse RSS Content
+				{showParseRssContent ? 'Hide RSS Parser' : 'Show RSS Parser'}
+			</button>
+			<button
+				class="px-4 py-2 rounded-lg border-2 text-white hover:bg-white cursor-pointer bg-red-500 border-red-500 hover:text-red-500"
+				onclick={() => setShowFilters(!showFilters)}
+			>
+				{showFilters ? 'Hide Filters' : 'Show Filters'}
 			</button>
 		</div>
-		{#if parseError}
-			<div class="text-red-500">{parseError}</div>
-		{/if}
-	</div>
-{/if}
-
-<!-- MARK: - Filters -->
-
-{#if showFilters}
-	<div class="max-w-4xl mx-auto my-6 p-4 border rounded shadow">
-		<h2 class="text-xl font-bold mb-4">Filters</h2>
-
-		<div class="flex flex-col gap-y-4">
-			<div>
-				<label class="block mb-2 font-semibold" for="company">Company</label>
-				<input
-					type="text"
-					id="company"
-					name="company"
-					placeholder="Enter company name to filter by"
-					class="ml-2 p-2 border rounded w-full"
-					bind:value={company}
-				/>
-			</div>
-
-			<div>
-				<label class="block mb-2 font-semibold" for="tags">Tags</label>
-				<input
-					type="text"
-					id="tags"
-					name="tags"
-					placeholder="Enter tags to filter by (comma-separated)"
-					class="ml-2 p-2 border rounded w-full"
-					bind:value={tags}
-				/>
-			</div>
+		<div class="flex gap-x-4">
+			<a
+				href="https://larajobs.com"
+				class="hover:underline cursor-pointer"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Visit LaraJobs
+			</a>
 		</div>
 	</div>
-{/if}
 
-<!-- List -->
-<div class="my-6">
-	<h1 class="text-2xl font-bold text-center mb-4">Job Postings</h1>
-</div>
+	<!-- MARK: - RSS Parser -->
 
-<div class="text-center mb-4">
-	Total: {$postings ? $postings.length : 0} job posting{$postings && $postings.length !== 1
-		? 's'
-		: ''}
-</div>
-
-<div>
-	{#if $postings && $postings.length === 0}
-		<p class="text-center">
-			No job postings available. Please add some using the RSS parser above.
-		</p>
-	{/if}
-</div>
-
-<ul class="max-w-4xl mx-auto">
-	{#if $postings}
-		{#each $postings as posting (posting.id)}
-			<li class="mb-4 p-4 border rounded shadow flex gap-x-4 items-center">
-				<div>
-					{#if posting.company_logo !== ''}
-						<img
-							src={posting.company_logo}
-							alt="{posting.company} Logo"
-							class="h-16 mb-2"
-							width="80px"
-							height="100px"
-						/>
-					{/if}
-				</div>
-				<div>
-					<h2 class="text-xl font-bold">{posting.name} at {posting.company}</h2>
-					<p>Location: {posting.location}</p>
-					<p>Type: {posting.job_type}</p>
-					<p>Salary: {posting.salary === '' ? 'Not specified' : posting.salary}</p>
-					<p>Posted: {getDateInRelativeFormat(posting.parsed_published_date)}</p>
-					<a href={posting.link} class="text-blue-500 hover:text-blue-700" target="_blank"
-						>View Job Posting</a
+	{#if showParseRssContent}
+		<div class="max-w-4xl mx-auto my-6 p-4 border rounded shadow">
+			<h2 class="text-xl font-bold mb-4">Instructions to Parse RSS Feed</h2>
+			<ol class="list-decimal list-inside space-y-2">
+				<li>
+					Go to <a
+						href="https://larajobs.com/feed"
+						class="text-blue-500 hover:text-blue-700"
+						target="_blank">https://larajobs.com/feed</a
 					>
-					{#if posting.tags.length > 0 && posting.tags[0]}
-						<div class="mt-2">
-							{#each posting.tags as tag}
-								<span
-									class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-									>#{tag}</span
-								>
-							{/each}
-						</div>
-					{/if}
-				</div>
-				<div class="ml-auto">
-					<!-- svelte-ignore a11y_consider_explicit_label -->
+				</li>
+				<li>
+					Copy all the contents under "This XML file does not appear to have any style information
+					associated with it. The document tree is shown below."
+				</li>
+				<li>Paste the content into the text box below</li>
+				<li>Click "Parse RSS Content" to add the job postings</li>
+			</ol>
+		</div>
+		<div class="my-6 text-center flex flex-col gap-y-2">
+			<div>
+				<textarea
+					class="bg-gray-100 dark:bg-gray-700"
+					bind:value={content}
+					rows="4"
+					cols="80"
+					placeholder="Paste RSS feed content here"
+				></textarea>
+			</div>
+			<div>
+				<button
+					class="px-6 py-4 rounded-lg border-2 text-white hover:bg-white bg-red-500 cursor-pointer border-red-500 hover:text-red-500"
+					onclick={parseRssContent}
+				>
+					Parse RSS Content
+				</button>
+			</div>
+			{#if parseError}
+				<div class="text-red-500">{parseError}</div>
+			{/if}
+		</div>
+	{/if}
+
+	<!-- MARK: - Filters -->
+
+	{#if showFilters}
+		<div class="max-w-4xl mx-auto my-6 p-4 border rounded shadow">
+			<div class="flex justify-between items-center mb-4">
+				<h2 class="text-xl font-bold mb-4">Filters</h2>
+				<div class="mb-4 flex gap-x-4">
 					<button
 						class="px-4 py-2 rounded-lg border-2 text-white hover:bg-white cursor-pointer bg-red-500 border-red-500 hover:text-red-500"
-						onclick={() => deletePosting(posting.id!)}
+						onclick={applyFilters}
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="size-6"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-							/>
-						</svg>
+						Apply Filters
+					</button>
+					<button
+						class="px-4 py-2 rounded-lg border-2 text-white hover:bg-white cursor-pointer bg-gray-500 border-gray-500 hover:text-gray-500"
+						onclick={() => {
+							company = '';
+							tags = '';
+							applyFilters();
+						}}
+					>
+						Clear Filters
 					</button>
 				</div>
-			</li>
-		{/each}
+			</div>
+
+			<div class="flex flex-col gap-y-4">
+				<div>
+					<label class="block mb-2 font-semibold" for="company">Company</label>
+					<input
+						type="text"
+						id="company"
+						name="company"
+						placeholder="Enter company name to filter by"
+						class="ml-2 p-2 border rounded w-full dark:bg-gray-700 dark:text-white"
+						bind:value={company}
+						onkeydown={(e) => e.key === 'Enter' && applyFilters()}
+					/>
+				</div>
+
+				<div>
+					<label class="block mb-2 font-semibold" for="tags">Tags</label>
+					<input
+						type="text"
+						id="tags"
+						name="tags"
+						placeholder="Enter tags to filter by (comma-separated). To filter by no tags, enter 'none'."
+						class="ml-2 p-2 border rounded w-full dark:bg-gray-700 dark:text-white"
+						bind:value={tags}
+						onkeydown={(e) => e.key === 'Enter' && applyFilters()}
+					/>
+				</div>
+
+				<div>
+					<label class="block mb-2 font-semibold" for="published-date">Published Date</label>
+					<input
+						type="text"
+						disabled
+						class="ml-2 p-2 border rounded w-full dark:bg-gray-700 dark:text-white"
+						value="Filtered postings are sorted by published date in descending order."
+						id="published-date"
+						name="published-date"
+					/>
+				</div>
+			</div>
+		</div>
 	{/if}
-</ul>
+
+	<!-- MARK: - List -->
+	<div class="my-6">
+		<h1 class="text-2xl font-bold text-center mb-4">Job Postings</h1>
+	</div>
+
+	<div class="text-center mb-4">
+		Total: {$postings ? $postings.length : 0} job posting{$postings && $postings.length !== 1
+			? 's'
+			: ''}
+	</div>
+
+	<div>
+		{#if $postings && $postings.length === 0}
+			<p class="text-center">
+				No job postings available. Please add some using the RSS parser above.
+			</p>
+		{/if}
+	</div>
+
+	<ul class="max-w-4xl mx-auto list-none p-0 dark:bg-gray-800">
+		{#if $postings}
+			{#each $postings as posting (posting.id)}
+				<li class="mb-4 p-4 border rounded shadow flex gap-x-4 items-center">
+					<div>
+						{#if posting.company_logo !== ''}
+							<img
+								src={posting.company_logo}
+								alt="{posting.company} Logo"
+								class="h-16 mb-2"
+								width="80px"
+								height="100px"
+							/>
+						{/if}
+					</div>
+					<div>
+						<h2 class="text-xl font-bold">{posting.name} at {posting.company}</h2>
+						<p>Location: {posting.location}</p>
+						<p>Type: {posting.job_type}</p>
+						<p>Salary: {posting.salary === '' ? 'Not specified' : posting.salary}</p>
+						<p>Posted: {getDateInRelativeFormat(posting.parsed_published_date)}</p>
+						<a href={posting.link} class="text-blue-500 hover:text-blue-700" target="_blank"
+							>View Job Posting</a
+						>
+						{#if posting.tags.length > 0 && posting.tags[0]}
+							<div class="mt-2">
+								{#each posting.tags as tag}
+									<span
+										class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+										>#{tag}</span
+									>
+								{/each}
+							</div>
+						{/if}
+					</div>
+				</li>
+			{/each}
+		{/if}
+	</ul>
+{/if}
