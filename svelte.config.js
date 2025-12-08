e4df6ca3be9717c58093e75055c49373db4056c8
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-07 21:47:04 -0500

    Add dotenv dependency and configure environment variables in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,36 +1,41 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import dotenv from 'dotenv';
 
-const isVercel = !!process.env.IS_VERCEL;
+dotenv.config();
 
 function getAdapter() {
-  if (isVercel) {
-    return adapter({});
-  }
+	const selectedAdapter = process.env.ADAPTER || 'static';
 
-  return adapter({
-    pages: 'build',
-    assets: 'build',
-    fallback: '404.html',
-    precompress: false,
-    strict: true
-  });
+	console.log(selectedAdapter);
+
+	if (selectedAdapter === 'auto') {
+		return adapter();
+	}
+
+	return adapter({
+		pages: 'build',
+		assets: 'build',
+		fallback: '404.html',
+		precompress: false,
+		strict: true
+	});
 }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: getAdapter(),
-    paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    }
-  }
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: getAdapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
