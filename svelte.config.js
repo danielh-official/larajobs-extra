e84433be63b17b8d1694065240514f4de52662e7
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-07 21:53:06 -0500

    Refactor adapter selection logic in svelte.config.js for clarity and consistency

--- svelte.config.js
+++ svelte.config.js
@@ -1,41 +1,42 @@
-import adapter from '@sveltejs/adapter-static';
+import staticAdapter from '@sveltejs/adapter-static';
+import autoAdapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 function getAdapter() {
-	const selectedAdapter = process.env.ADAPTER || 'static';
+  const selectedAdapter = process.env.ADAPTER || 'static';
 
-	console.log(selectedAdapter);
+  console.log("The current selected adapter is:", selectedAdapter);
 
-	if (selectedAdapter === 'auto') {
-		return adapter();
-	}
+  if (selectedAdapter === 'auto') {
+    return autoAdapter();
+  }
 
-	return adapter({
-		pages: 'build',
-		assets: 'build',
-		fallback: '404.html',
-		precompress: false,
-		strict: true
-	});
+  return staticAdapter({
+    pages: 'build',
+    assets: 'build',
+    fallback: '404.html',
+    precompress: false,
+    strict: true
+  });
 }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: getAdapter(),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: getAdapter(),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
+  }
 };
 
 export default config;
